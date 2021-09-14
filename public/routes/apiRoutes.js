783d45ba440d27941a12fd677d7a112990a5c0ba
scampos321 <sebastian.campos321@gmail.com>
2021-09-14 18:06:55 -0400

    added server file and added routes for notes html

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const fs = require("fs");
+const uuid = require("uuid");
 
 router.get("/notes", (req, res) => {
-    res.send("Hello World!");
-});
+    const noteData = fs.readFileSync("./db/db.json");
+    res.json(JSON.parse(noteData));
+})
+
+router.post("/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const createNote = req.body;
+    createNote.id = uuid.v4();
+    notes.push(createNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes);
+})
+
+router.delete("/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const deleteNote = notes.filter((ersNote) => ersNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote));
+    res.json(deleteNote);
+})
 
 module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const apiRoute = require('./public/routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use("/api", apiRoute)
+
+app.get("/notes", (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html");
+  });
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.info(`Listening on PORT: https://localhost:${PORT}`)
+);
